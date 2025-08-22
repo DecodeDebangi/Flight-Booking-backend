63d35a3fbccf1426c9d7e60607286a5755a4f926
Debangi <debangiofficial@gmail.com>
2025-08-22 13:54:10 +0530

    added a new city model

---
+++ src/models/city.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class City extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  City.init(
+    {
+      name: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1, 50],
+      },
+    },
+    {
+      sequelize,
+      modelName: "City",
+    }
+  );
+  return City;
+};
